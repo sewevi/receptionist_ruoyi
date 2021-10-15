e9dbca6a3b21d943ece88d3306ef9137f8511270
xdayi <1065453415@qq.com>
2021-10-15 16:04:51 +0800

    添加支付模块

---
+++ ruoyi-receptionist/src/main/java/com/receptionist/service/AliPayService.java
@@ -0,0 +1,31 @@
+package com.receptionist.service;
+
+import com.receptionist.entity.alipay.AliPayResponse;
+
+/**
+ * @author xdayi@linewell.com
+ * @Description
+ * @date 2021/5/14
+ */
+public interface AliPayService {
+    /**
+     * 查询账户剩余余额
+     * @param aliPayUserId aliPay用户主键
+     * @return
+     */
+    AliPayResponse accountQuery(String aliPayUserId);
+
+    /**
+     * 扫码支付
+     * @param paymentValue
+     * @return
+     */
+    String tradePreCreatePay(String paymentValue);
+
+    /**
+     * 单笔转账到支付宝账户
+     * @param paymentValue
+     * @return
+     */
+    String transfer(String paymentValue);
+}
